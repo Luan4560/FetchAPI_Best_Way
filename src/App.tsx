071d3de383b9f58e5494ccecc767a0225f59a1db
Luan <9001545@riachuelo.com.br>
2022-02-16 19:00:25 -0300

    invalidateCache

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import { Repo } from "./pages/Repo";
+import { Repos } from "./pages/Repos";
 
-type Repository = {
-  full_name: string;
-  description: string;
-}
-
-function App() {
-  const { data: repositories, isFetching } = useFetch<Repository[]>('users/luan4560/repos')
-
+export function App() {
   return (
-   <ul>
-     {isFetching && <p>Carregando...</p>}
-     {repositories?.map(repo =>  (
-       <li key={repo.full_name}>
-         <strong>{repo.full_name}</strong>
-         <p>{repo.description}</p>
-       </li>
-     ))}
-   </ul>
+    <Routes>
+      <Route path="/"  element={<Repos/>} />
+      <Route path="/repos/*"  element={<Repo />} />
+    </Routes>
   )
 }
-
-export default App
